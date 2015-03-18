6328bc71686865386e2f0a1f33d7e82e773e724a
Dan Buch <daniel.buch@gmail.com>
2015-03-18 10:42:12 -0400

    Wipe

--- .travis.rb
+++
@@ -1,8 +0,0 @@
-{
-  language: 'ruby',
-  sudo: false,
-  ruby: %w[2.1.4 1.9.3],
-  script: <<-EOF
-    ruby wat.rb
-  EOF
-}
--- src/java/com/meatballhat/cloaked/octo/tyrion/Whatever.java
+++
@@ -1,7 +0,0 @@
-package com.meatballhat.cloaked.octo.tyrion;
-
-class Whatever {
-  public static void main(String[] args) {
-    System.out.println("OHAI!");
-  }
-} 
